8ac1832a3e34fc59ef2033c427f03415190c6660
Joshua Faskowitz <jfaskowi@iu.edu>
2020-01-03 11:51:30 -0500

    enable tools to not have to provide dwi--in case of tracking

--- src/dw_utils/cmdline.py
+++ src/dw_utils/cmdline.py
@@ -19,30 +19,26 @@ class CmdLineHandler:
         self.parser = argparse.ArgumentParser(description=parserdesc)
 
     def dwibasics(self):
-
         # this is the whole dwi with all the volumes yo
-        self.parser.add_argument('-dwi', nargs='?', required=True,
+        self.parser.add_argument('-dwi', nargs='?', required=False,
                                  help="path to dwi yo")
-
         # this is an easy binary mask that you should aready have
         self.parser.add_argument('-mask', nargs='?', required=True,
                                  help="path to mask yo")
-
         # this is the bvecs file, which should be eddy and flirt rotated
         self.parser.add_argument('-bvec', nargs='?', required=True,
                                  help="path to bvecs file yo")
-
         # this is the bval, which sometimes is the same for the whole dataset
         self.parser.add_argument('-bval', nargs='?', required=True,
                                  help="path to bvals file yo")
-
         # the output should be provided, but I guess it could be optional
         self.parser.add_argument('-output', nargs='?',
                                  help="Add the name of the output prefix you want")
 
     def checkdwibasics(self):
 
-        checkisfile(self.dwi_)
+        if self.dwi_:
+            checkisfile(self.dwi_)
         checkisfile(self.mask_)
         checkisfile(self.bval_)
         checkisfile(self.bvec_)
